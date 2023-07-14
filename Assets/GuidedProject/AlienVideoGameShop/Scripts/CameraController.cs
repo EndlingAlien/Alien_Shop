5a80d48f98fe2e5c54c6289bf3d6216bb169cf83
Asa <78871911+EndlingAlien@users.noreply.github.com>
2023-07-14 13:07:32 -0600

    Renovated Store
    New Cameras positioned around scene, controlled with Camera Controller. Removed West wing room Diner, west light probes, imported diner prefabs.
    Remade Solar System Prefab, added new planets, updated respective scripts.

--- Assets/GuidedProject/AlienVideoGameShop/Scripts/CameraController.cs
+++ Assets/GuidedProject/AlienVideoGameShop/Scripts/CameraController.cs
@@ -7,6 +7,8 @@ public class CameraController : MonoBehaviour
     public GameObject camOne;
     public GameObject camTwo;
     public GameObject camThree;
+    public GameObject camFour;
+    public GameObject camFive;
 
     // Start is called before the first frame update
     void Start()
@@ -14,6 +16,8 @@ public class CameraController : MonoBehaviour
         camOne.SetActive(true);
         camTwo.SetActive(false);
         camThree.SetActive(false);
+        camFour.SetActive(false);
+        camFive.SetActive(false);
     }
 
     // Update is called once per frame
@@ -24,18 +28,40 @@ public class CameraController : MonoBehaviour
             camOne.SetActive(true);
             camTwo.SetActive(false);
             camThree.SetActive(false);
+            camFour.SetActive(false);
+            camFive.SetActive(false);
         }
         else if (Input.GetKeyDown(KeyCode.W))
         {
             camOne.SetActive(false);
             camTwo.SetActive(true);
             camThree.SetActive(false);
+            camFour.SetActive(false);
+            camFive.SetActive(false);
         }
         else if (Input.GetKeyDown(KeyCode.E))
         {
             camOne.SetActive(false);
             camTwo.SetActive(false);
             camThree.SetActive(true);
+            camFour.SetActive(false);
+            camFive.SetActive(false);
+        }
+        else if (Input.GetKeyDown(KeyCode.R))
+        {
+            camOne.SetActive(false);
+            camTwo.SetActive(false);
+            camThree.SetActive(false);
+            camFour.SetActive(true);
+            camFive.SetActive(false);
+        }
+        else if (Input.GetKeyDown(KeyCode.T))
+        {
+            camOne.SetActive(false);
+            camTwo.SetActive(false);
+            camThree.SetActive(false);
+            camFour.SetActive(false);
+            camFive.SetActive(true);
         }
     }
 }
--- Assets/GuidedProject/AlienVideoGameShop/Scripts/SolarSystem.cs
+++ Assets/GuidedProject/AlienVideoGameShop/Scripts/SolarSystem.cs
@@ -6,24 +6,38 @@ public class SolarSystem : MonoBehaviour
 {
 
     public GameObject sun;
-    public GameObject planetOne;
-    public GameObject planetTwo;
-    public GameObject planetThree;
-    public GameObject planetFour;
-    public GameObject planetFive;
-    public GameObject planetSix;
-    public GameObject planetSeven;
+    public GameObject P1;
+    public GameObject P2;
+    public GameObject P3;
+    public GameObject P4;
+    public GameObject P5;
+    public GameObject P6;
+    public GameObject P7;
+    public GameObject P8;
+    public GameObject P9;
+    public GameObject P10;
+    public GameObject P11;
+    public GameObject P12;
+    public GameObject P13;
+    public GameObject P14;
 
     // Update is called once per frame
     void Update()
     {
-        planetOne.transform.RotateAround(sun.transform.position, Vector3.up, 25 * Time.deltaTime);
-        planetTwo.transform.RotateAround(sun.transform.position, Vector3.up, 45 * Time.deltaTime);
-        planetThree.transform.RotateAround(sun.transform.position, Vector3.up, 30 * Time.deltaTime);
-        planetFour.transform.RotateAround(sun.transform.position, Vector3.up, 10 * Time.deltaTime);
-        planetFive.transform.RotateAround(sun.transform.position, Vector3.up, 35 * Time.deltaTime);
-        planetSix.transform.RotateAround(sun.transform.position, Vector3.up, 20 * Time.deltaTime);
-        planetSeven.transform.RotateAround(sun.transform.position, Vector3.up, 5 * Time.deltaTime);
+        P1.transform.RotateAround(sun.transform.position, Vector3.up, 20 * Time.deltaTime);
+        P2.transform.RotateAround(sun.transform.position, Vector3.up, 18 * Time.deltaTime);
+        P3.transform.RotateAround(sun.transform.position, Vector3.up, 4 * Time.deltaTime);
+        P4.transform.RotateAround(sun.transform.position, Vector3.up, 10 * Time.deltaTime);
+        P5.transform.RotateAround(sun.transform.position, Vector3.up, 20 * Time.deltaTime);
+        P6.transform.RotateAround(sun.transform.position, Vector3.up, 23 * Time.deltaTime);
+        P7.transform.RotateAround(sun.transform.position, Vector3.up, 16 * Time.deltaTime);
+        P8.transform.RotateAround(sun.transform.position, Vector3.up, 10 * Time.deltaTime);
+        P9.transform.RotateAround(sun.transform.position, Vector3.up, 7 * Time.deltaTime);
+        P10.transform.RotateAround(sun.transform.position, Vector3.up, 7 * Time.deltaTime);
+        P11.transform.RotateAround(sun.transform.position, Vector3.up, 12 * Time.deltaTime);
+        P12.transform.RotateAround(sun.transform.position, Vector3.up, 18 * Time.deltaTime);
+        P13.transform.RotateAround(sun.transform.position, Vector3.up, 14 * Time.deltaTime);
+        P14.transform.RotateAround(sun.transform.position, Vector3.up, 4 * Time.deltaTime);
     }
 
 }
