07102d0289229ae669b9a1458b874b01704c10c9
Asa <78871911+EndlingAlien@users.noreply.github.com>
2023-07-15 21:15:26 -0600

    Animated Male Alien
    Created script for random animations

---
+++ Assets/GuidedProject/AlienVideoGameShop/Scripts/ClarkAnimSelect.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClarkAnimSelect : StateMachineBehaviour
+{
+    private bool turnLeftFinished = false;
+
+    // Pick a random number each time you enter a new state to randomize anims
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.SetInteger("LookBelowSelect", Random.Range(1, 2));
+        animator.SetInteger("LookAboveSelect", Random.Range(1, 4));
+        animator.SetInteger("StretchSelect", Random.Range(1, 3));
+        animator.SetInteger("MusicSelect", Random.Range(1, 4));
+    }
+}
+
